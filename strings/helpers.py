95859f6afb4d501f866b7c448c744f7f80a41e0a
NekoLocal <132919248+nekolocal@users.noreply.github.com>
2023-07-15 21:36:34 +0700

    Create helpers.py

---
+++ strings/helpers.py
@@ -0,0 +1,53 @@
+HELP_1 = """🙄**<u>ADMIN COMMANDS:</u>**
+
+Hanya menambahkan **/c** di awal perintah untuk menggunakannya di saluran.
+
+/pause : Jeda streaming yang sedang diputar.
+/resume : Lanjutkan streaming yang telah dijeda.
+/mute : Matikan suara streaming yang sedang diputar.
+/unmute : Hidupkan suara streaming yang telah dimatikan.
+/skip : Lewati streaming yang sedang diputar dan mulai streaming lagu berikutnya di antrian.
+/end or /stop : Hapus antrian dan akhiri streaming yang sedang diputar.
+/shuffle : Acak urutan lagu dalam antrian.
+/seek : Geser streaming ke durasi yang ditentukan.
+/seekback : Mundurkan streaming ke durasi yang ditentukan.
+/reboot : Restart bot dalam obrolan Anda.
+
+🥴<u>**LOOP PLAY:**</u>
+
+/loop [DISABLE/ENABLE] or [1:10] 
+    : Ketika diaktifkan, bot akan memutar streaming yang sedang diputar dalam loop sebanyak 10 kali atau sesuai jumlah loop yang diminta.
+
+😜<u>**AUTH USERS:**</u>
+
+Pengguna yang diautorisasi dapat menggunakan hak admin dalam bot tanpa memiliki hak admin di obrolan.
+
+/auth [username] : Tambahkan pengguna ke daftar pengguna yang diautorisasi bot.
+/unauth [username] : Hapus pengguna dari daftar pengguna yang diautorisasi.
+/authusers : Tampilkan daftar pengguna yang diautorisasi dalam grup.
+**Powered by** @NekoLocal."""
+
+HELP_2 = """💞<u>**Daftar Perintah Putar Musik:**</u>
+
+Perintah Tersedia: play, vplay, cplay
+
+Perintah Putar Paksa: playforce, vplayforce, cplayforce
+
+**c** adalah singkatan dari "channel play" (putar saluran).
+**v** adalah singkatan dari "video play" (putar video).
+**force** adalah singkatan dari "force play" (putar paksa).
+
+/play atau /vplay atau /cplay: Memulai streaming lagu yang diminta di video chat.
+
+/playforce atau /vplayforce atau /cplayforce: **Putar paksa** menghentikan streaming yang sedang berlangsung dan memulai streaming lagu yang diminta.
+
+/channelplay [nama pengguna atau ID obrolan] atau [disable]: Menyambungkan saluran ke grup dan memulai streaming lagu dengan bantuan perintah yang dikirim di grup.
+
+
+🤨**<u>Daftar Putar Server:</u>**
+
+/playlist: Periksa daftar putar yang disimpan di server.
+/deleteplaylist: Hapus lagu yang disimpan dalam daftar putar.
+/play: Memulai memutar lagu dari daftar putar yang disimpan di server.
+**Powered by** @NekoLocal."""
+
