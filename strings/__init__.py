385e5df710991abe9f9cecb16925f521202f33f5
NekoLocal <132919248+nekolocal@users.noreply.github.com>
2023-07-15 21:22:05 +0700

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,14 +1,3 @@
-#
-# Copyright (C) 2021-2022 by Alexa_Help@Github, < https://github.com/Jankarikiduniya >.
-# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
-
-# Kanged By © @Dr_Asad_Ali
-# Rocks © @Shayri_Music_Lovers
-# Owner Asad Ali
-# Harshit Sharma
-# All rights reserved. © Alisha © Alexa © Yukki
-
-
 import os
 from typing import List
 
@@ -29,23 +18,25 @@ def get_string(lang: str):
 for filename in os.listdir(r"./strings"):
     if filename.endswith(".yml"):
         language_name = filename[:-4]
+        if language_name == "en":
+            language_name = "id"
         commands[language_name] = yaml.safe_load(
             open(r"./strings/" + filename, encoding="utf8")
         )
 
 
 for filename in os.listdir(r"./strings/langs/"):
-    if "en" not in languages:
-        languages["en"] = yaml.safe_load(
-            open(r"./strings/langs/en.yml", encoding="utf8")
+    if "id" not in languages:
+        languages["id"] = yaml.safe_load(
+            open(r"./strings/langs/id.yml", encoding="utf8")
         )
     if filename.endswith(".yml"):
         language_name = filename[:-4]
-        if language_name == "en":
+        if language_name == "id":
             continue
         languages[language_name] = yaml.safe_load(
             open(r"./strings/langs/" + filename, encoding="utf8")
         )
-        for item in languages["en"]:
+        for item in languages["id"]:
             if item not in languages[language_name]:
-                languages[language_name][item] = languages["en"][item]
+                languages[language_name][item] = languages["id"][item]
