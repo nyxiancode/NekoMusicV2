3d785aa471068bc4f2bf0f028741fc3b2c0a8e03
NekoLocal <132919248+nekolocal@users.noreply.github.com>
2023-07-16 19:33:01 +0700

    Create __init__.py

---
+++ NekoMusic/__init__.py
@@ -0,0 +1,38 @@
+from NekoMusic.core.bot import AlexaBot
+from NekoMusic.core.dir import dirr
+from NekoMusic.core.git import git
+from NekoMusic.core.userbot import Userbot
+from NekoMusic.misc import dbb, heroku, sudo
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = AlexaBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
