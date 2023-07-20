7892b28a419efa2918af920e8be0bcd4082e45e7
NekoLocal <132919248+nekolocal@users.noreply.github.com>
2023-07-20 11:10:30 +0700

    Create resume.py

---
+++ NekoMusic/plugins/admins /resume.py
@@ -0,0 +1,33 @@
+# Kanged By © @Dr_Asad_Ali
+# Rocks © @Shayri_Music_Lovers
+# Owner Asad Ali
+# Harshit Sharma
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import BANNED_USERS
+from strings import get_command
+from NekoMusic import app
+from NekoMusic.core.call import Neko
+from NekoMusic.utils.database import is_music_playing, music_on
+from NekoMusic.utils.decorators import AdminRightsCheck
+
+# Commands
+RESUME_COMMAND = get_command("RESUME_COMMAND")
+
+
+@app.on_message(
+    filters.command(RESUME_COMMAND) & filters.group & ~filters.edited & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def resume_com(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return await message.reply_text(_["general_2"])
+    if await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_3"], disable_web_page_preview=True)
+    await music_on(chat_id)
+    await Neko.resume_stream(chat_id)
+    await message.reply_text(
+        _["admin_4"].format(message.from_user.mention), disable_web_page_preview=True
+    )
